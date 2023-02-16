f1c7e70dbad000b04a2305aa715faba7f5315929
taylorc21 <cameron.taylor@salesianum.org>
2023-02-16 14:54:33 -0500

    second commit

--- a.java
+++ a.java
@@ -1,11 +1,11 @@
 public class a{
     String[][] letterBlock;
 
-    public void fillBlock(String str){
-        for (int rows = 0; rows < letterBlock.length; rows++){
+    public void fillBlock(String str, String[][] letterBlock){
+        for (int row = 0; row < letterBlock.length; row++){
             for (int col = 0; col < letterBlock[0].length; col++){
-                
-                letterBlock = new String[][];
+                letterBlock[row][col] = str.substring(row, row + 1);
+              
             }
 
         }
