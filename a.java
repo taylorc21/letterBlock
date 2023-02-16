a7d8d9163864d1453fcce245607478955bbdedca
taylorc21 <cameron.taylor@salesianum.org>
2023-02-16 14:46:02 -0500

    first commit

---
+++ a.java
@@ -0,0 +1,15 @@
+public class a{
+    String[][] letterBlock;
+
+    public void fillBlock(String str){
+        for (int rows = 0; rows < letterBlock.length; rows++){
+            for (int col = 0; col < letterBlock[0].length; col++){
+                
+                letterBlock = new String[][];
+            }
+
+        }
+
+    }
+
+}
