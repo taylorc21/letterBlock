428fa7d41d47ecb2d202c246256f110a3dcc743e
taylorc21 <cameron.taylor@salesianum.org>
2023-03-16 11:23:34 -0400

    first commit

---
+++ cipher.java
@@ -0,0 +1,51 @@
+public class cipher{
+    String[][] letterBlock;
+    private int numRows;
+    private int numCols;
+
+    public void fillBlock(String str, String[][] letterBlock){
+        
+        if (str.length() < numRows * numCols){
+            int count = 0;
+
+            for (int row = 0; row < letterBlock.length; row++){
+                for (int col = 0; col < letterBlock[0].length; col++){
+                    count ++;
+
+                    if (count < str.length()){
+                        letterBlock[row][col] = str.substring(count, count + 1);
+                    }
+
+                    else{
+                        letterBlock[row][col] = "A";
+                    }
+                }
+            }
+        }       
+
+        if (str.length() > numRows * numCols){
+
+            int iteration = 0;
+
+            for (int row = 0; row > letterBlock.length; row++){
+                for (int col = 0; col < letterBlock[0].length; col++){
+                    letterBlock[row][col] = str.substring(iteration, iteration + 1);
+                    iteration++;
+                }
+            }
+
+        }
+    }
+
+     public String encryptMessage (String message){
+        String encryptMSG = "";
+        String temp;
+        for (int i = 0; i < letterBlock[0].length; i++){
+            temp = letterBlock[0][i].encryptBlock(message);
+            encryptMSG += temp;
+
+        }
+
+        return encryptMSG;
+     }
+}
